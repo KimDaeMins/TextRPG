dfc7aa842f7661e02b04336c4db71ef0c86114b0
KimDaeMins <eoals81712@gmail.com>
2023-08-19 06:45:48 +0900

    Show Garget 만들어야함 이거하고 오류테스트도 하고 하나씩 해보자

---
+++ TextRPG/Main/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace TextRPG
+{
+    class Program
+    {
+        GameObject go = new GameObject();
+        static void Main(string[] args)
+        {
+            Monster monster = new Monster();
+            Console.WriteLine("Hello world");
+        }
+    }
+}
