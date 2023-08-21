55a99cec73560b9599822221b4272bb15097b14f
KimDaeMins <eoals81712@gmail.com>
2023-08-21 10:42:39 +0900

    no message

---
+++ TextRPG/Enum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG
+{
+    public enum ItemKinds
+    {
+        Equips, Consumables, Others , End
+    }
+    public enum WearTypes
+    {
+        Head, Body, Arm, Leg, Boots, End
+    }
+    public enum WeaponTypes
+    {
+        Bow, OneHandSword, TwoHandSword, Staff, Shield
+    }
+}
---
+++ TextRPG/GO/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG
+{
+    internal class Player : GameObject
+    {
+        public void Init()
+        {
+            _gadget = new Gadget();
+            _inventory = new Inventory();
+        }
+
+
+
+
+        private Gadget _gadget;
+        private Inventory _inventory;
+    }
+}
